e27fe1ca625548d416bad73c8d7811d81105be86
suprajashetty <suprajashetty84@gmail.com>
2026-03-08 18:25:26 +0530

    Update settings.py

--- mobile_shop_backend-main/mobile_shop_backend/settings.py
+++ mobile_shop_backend-main/mobile_shop_backend/settings.py
@@ -16,21 +16,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-kv$uiy3h+g$*o@_izyya*ct_y0%6)1*wg++tth-pdp$upe(%$7'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# Set to False in production
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = ['*']  # or specify your Render URL: ['mobileshopmain-4.onrender.com']
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,13 +32,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',  # Your app
-    'whitenoise.runserver_nostatic',  # For serving static files in production
+    'website',  # your app
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # serve static files in production
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -58,7 +51,7 @@ ROOT_URLCONF = 'mobile_shop_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  # Add custom template dirs if needed
+        'DIRS': [BASE_DIR / 'templates'],  # Add custom template dir if needed
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,10 +66,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mobile_shop_backend.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -84,56 +74,29 @@ DATABASES = {
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
-
-# Directory where collectstatic will gather files for production
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# Additional directories to look for static files during development
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic
 STATICFILES_DIRS = [
-    BASE_DIR / 'mobile_shop_backend' / 'static',  # <-- Correct path for your images/js/css
+    BASE_DIR / 'static',  # Your local static files folder
 ]
 
 # WhiteNoise storage for caching and compression
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
